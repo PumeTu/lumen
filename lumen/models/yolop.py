038d80e451c0d1d8069e779e40bfc6721a90ee68
Pume Tuchinda <pumetuchinda@gmail.com>
2023-02-23 05:29:20 -0500

    add YOLOv4 (still need to update forward pass)

--- lumen/models/yolop.py
+++ lumen/models/yolop.py
@@ -1,7 +1,11 @@
 import torch
 import torch.nn as nn
+
+from typing import List
+
 from lumen.layers.conv import DepthWiseSeperableConv, ConvBnAct
 from .darknet import CSPLayer
+from lumen.utils.utils import multi_apply
 
 class PANet(nn.Module):
     """Path Aggregation Network
@@ -52,15 +56,35 @@ class PANet(nn.Module):
 class YOLOv4Head(nn.Module):
     """YOLOv4 Anchor based Head
     """
-    def __init__(self):
+    def __init__(self, 
+                in_channels: tuple = (256, 512, 1024),
+                num_classes: int = 80,
+                anchors: List = [[[12, 16], [19, 36], [40, 28]],
+                                [[36, 75], [76, 55], [72, 146]],
+                                [[142, 110], [192, 243], [459, 401]]]):
         super().__init__()
+        self.in_channels = in_channels
+        self.anchors = anchors
+        self.num_anchors = len(anchors)
+        self.num_outputs = 5 + num_classes
+        self.num_levels = len(in_channels)
 
     @staticmethod
     def _decode_bbox_to_xywh(bbox_pred, anchor):
         raise NotImplementedError
 
     def forward(self, x):
-        raise NotImplementedError
+        assert len(x) == self.num_levels
+
+    def forward_single(self, x: torch.Tensor, conv: nn.Module):
+        """Forward fetaure of single scale"""
+        out = conv(x)
+        bs, _, h, w = out.shape
+        out = out.view(bs, self.num_anchors, h, w, -1)
+        class_score = out[..., 5:]
+        bbox_pred = out[..., :4]
+        confidence_score = out[...,4:5]
+        return class_score, bbox_pred, confidence_score
 
 class YOLOP(nn.Module):
     """YOLO for Panoptic Driving Perception
---
+++ lumen/utils/utils.py
@@ -0,0 +1,7 @@
+import torch
+import numpy as np
+
+from functools import partial
+
+def multi_apply():
+    raise NotImplemented
