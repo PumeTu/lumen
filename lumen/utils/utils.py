cad36834fb481913db1bf341ee6077fa61a27340
Pume Tuchinda <pumetuchinda@gmail.com>
2023-02-23 08:30:22 -0500

    fill out multi_apply

--- lumen/utils/utils.py
+++ lumen/utils/utils.py
@@ -3,5 +3,16 @@ import numpy as np
 
 from functools import partial
 
-def multi_apply():
-    raise NotImplemented
+def multi_apply(fn, *args, **kwargs):
+    """Apply function to a list of arguments
+    Note:
+        This function applies the 'fn' to multiple inputs and map the outputs of the 'fn' into different list.
+        Each list contains the same type of outputs corresponding to different outputs
+    Args:
+        fn (function): function that will be applied to a list of arguments
+    Returns:
+        tuple(list): tuple containing multiple list, each list with the returned results of the function
+    """
+    pfn = partial(fn, **kwargs) if kwargs else fn
+    results = map(pfn, *args)
+    return tuple(map(list, zip(*results)))
