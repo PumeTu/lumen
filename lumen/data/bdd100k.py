bdb0fcf11cf9030598b6668067873d2feaf92824
Pume Tuchinda <pumetuchinda@gmail.com>
2023-02-28 02:03:45 -0500

    update object detection data loader (still needs to be tested)

--- lumen/data/bdd100k.py
+++ lumen/data/bdd100k.py
@@ -6,16 +6,17 @@ import pandas as pd
 from typing import List
 import cv2
 import numpy as np
-
-from utils.bbox import xyxy_to_xywh, norm_bbox
-
+import matplotlib.pyplot as plt
+from PIL import Image
 
 class BDD100k(data.Dataset):
     def __init__(self, root: str = '../bdd100k/', transforms: transforms = None, training: bool = False, input_size: tuple = (416, 416), anchors: List = [[[12, 16], [19, 36], [40, 28]],
                                                                                                                         [[36, 75], [76, 55], [72, 146]],
                                                                                                                         [[142, 110], [192, 243], [459, 401]]]):
+        super().__init__()
         self.root = root
         self.training = training
+        self.transforms = transforms
         self.anchors = torch.tensor(anchors)
         self.scale = len(anchors)
         self.input_size = input_size
@@ -63,53 +64,19 @@ class BDD100k(data.Dataset):
 
         lane_path = self.lane_path + target['name'].replace('.jpg', '.png')
         drivable_path = self.drivable_path + target['name'].replace('.jpg', '.png')
- 
-        labels = torch.Tensor(labels)
-        detections = self._build_yolo_targets(labels)
-        segmentations = self._build_seg_target(lane_path, drivable_path)
 
-        return image, detections, segmentations
+        target = {}
+        target['detections'] = torch.as_tensor(labels, dtype=torch.float32)
+        target['segmentations'] = self._build_seg_target(lane_path, drivable_path)
 
-    @staticmethod
-    def _iou(box, anchor):
-        intersection = torch.min(box[...,0], anchor[..., 0]) * torch.min(box[...,1], anchor[...,1])
-        union = box[..., 0] * box[..., 1] + anchor[..., 0] * anchor[...,1] - intersection
-        return intersection / union
-    
-    def _build_yolo_targets(self, labels):
-        """
-        Builds the groundtruth for a YOLO network given the bounding box and class labels
-        Args:
-            labels (Tensor): bounding box and class labels of the entire dataset
-                - shape: (N, 5)
-        Returns:
-            (det1, det2, det3): all 3 detection targets for the 3 scales
-                - shape: (n_anchors, h / 2**n, w / 2**n, n_output)
-                n_output: conf, x, y, w, h, classes
-        """
-        groundtruths = [torch.zeros((self.scale, int(self.input_size[0] / 2**x), int(self.input_size[1] / 2**x), self.num_outputs)) for x in reversed(range(3, 6))]
-        for label in labels:
-            iou_index = self._iou(label[:5], self.anchors).argmax()
-            scale = iou_index // 3
-            anchor_index = iou_index % 3
-            _, sy, sx, _ = groundtruths[scale].shape
-            bbox = norm_bbox(xyxy_to_xywh(label[:5]), *self.input_size)
-            i, j = int(sy * bbox[1]), int(sx * bbox[0])
-            if not groundtruths[scale][anchor_index, i, j, self.num_classes]:
-                groundtruths[scale][anchor_index, i, j, self.num_classes] = 1
-                y_cell, x_cell = sy * bbox[1] - i, sx * bbox[0] - j
-                w_cell, h_cell = bbox[2] * sx, bbox[3] * sy
-                box = torch.tensor([x_cell, y_cell, w_cell, h_cell])
-                groundtruths[scale][anchor_index, i, j, self.num_classes+1:self.num_classes+5] = box
-                obj_class = int(label[-1].item())
-                groundtruths[scale][anchor_index, i, j, obj_class] = 1
+        if self.transforms:
+            image = self.transforms(image)
 
-        return groundtruths
-                
+        return image, target
+       
     def _build_seg_target(self, lane_path, drivable_path):
-        """
-        Build groundtruth for  segmentation
-            - this will combine the lanes and drivable masks into one 
+        """Build groundtruth for  segmentation
+        Note: This combines the lanes and drivable masks into one 
         Args:
             lane_path (str): path to lane binary mask
             drivable_path (str): path to drivable binary mask
@@ -127,3 +94,7 @@ class BDD100k(data.Dataset):
         mask = np.concatenate((lane_mask, drivable_mask), axis=0)
         
         return torch.tensor(mask)
+    
+def collate_fn(batch):
+    imgs, targets = zip(*batch)
+    return torch.stack(imgs, 0), targets
